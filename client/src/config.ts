ac76f7591e6adf8c7ea84ac964c2955348c688b2
Justin Ng <justin@aletheon.co>
2021-05-28 00:22:44 -0700

    Prepend REACT_APP_ to env vars

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,9 @@
 const apiId = process.env.REACT_APP_API_ID;
 const apiStage = process.env.REACT_APP_API_STAGE;
 export const apiToken = process.env.REACT_APP_API_TOKEN;
-const domain = process.env.AUTH0_DOMAIN || '';
-const clientId = process.env.AUTH0_CLIENT_ID || '';
-const callbackUrl =process.env.AUTH0_CALLBACK_URL || '';
+const domain = process.env.REACT_APP_AUTH0_DOMAIN || '';
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
+const callbackUrl =process.env.REACT_APP_AUTH0_CALLBACK_URL || '';
 
 export const apiEndpoint = `${apiId}/${apiStage}`;
 
